eebe52aeb4e8f8085c2e629709275c91938786b4
Binay Chandra <binaychandra90@gmail.com>
2023-10-10 19:19:27 +0530

    modified app.py file

--- app.py
+++ app.py
@@ -234,6 +234,6 @@ def hometest():
         return redirect(url_for('data'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    # from waitress import serve
-    # serve(app, host="0.0.0.0", port=8080)
+    #app.run(debug=True)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
